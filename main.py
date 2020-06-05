32b3757f8c1c69d950021d995b0617985687e53c
Ser4ph2 <65906603+Ser4ph2@users.noreply.github.com>
2020-06-05 14:39:03 +0100

    Added torrent updates

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ try:
     import feedparser
     import discord
     import asyncio
+    from hashlib import md5
     from os import stat, listdir
 except ModuleNotFoundError:
     print("Missing a module. Please check discord.py is installed.")
@@ -29,6 +30,33 @@ def get_embed(data):
         embeds.append(embed)
     return embeds
 
+def get_embed_modified(data):
+    embeds = []
+    for each in data:
+        title = each["title"]
+        summary = each["summary"]
+        author = each["author"]
+        link = each["comments"]
+        time = each["published"].replace("+0000", "UTC")
+
+        embed = discord.Embed(title="Torrent Updated", colour=0x9400D3)
+        embed.add_field(name="Title", value=title, inline=False)
+        embed.add_field(name="Summary", value=summary, inline=False)
+        embed.add_field(name="Link", value=link, inline=False)
+        embed.add_field(name="Author", value=author, inline=False)
+        embed.add_field(name="Published", value=time, inline=False)
+        embed.set_footer(text="Made by Seraph2")
+        embeds.append(embed)
+    return embeds
+
+# returns an md5 hash made from the uploader and publish time of a torrent. this is used to check whether it really is a new torrent or an edit for a previous one.
+def get_identifier(entry):
+    time = entry["published"]
+    author = entry["author"]
+    combined = str(time) + str(author)
+    identifier = md5(combined.encode())
+    return identifier.hexdigest()
+
 class MyClient(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -40,14 +68,23 @@ class MyClient(discord.Client):
 
     async def announce_torrents(self):
         await self.wait_until_ready()
-        announce_channel = self.get_channel(713468778322853999) # this is the channel I used for testing, replace this with the channel ID you would like to use for announces
+        announce_channel = self.get_channel(715229124230250527) # this is the channel I used for testing, replace this with the channel ID you would like to use for announces
         while not self.is_closed():
             self.feed = feedparser.parse("https://aither.cc/rss/56.e99367ccad6938825c2e315feab20fc8")
             log = []
+            current_hashes = list(map(get_identifier, self.feed["entries"]))
+            hashes = []
+            # check if torrent log exists
             if "log.txt" in listdir():
                 with open("log.txt", "r") as f:
                     for each in f.read().split("\n\n"):
                         log.append(each)
+                #check if torrent hashes exist
+                if "hashes.txt" in listdir():
+                    with open("hashes.txt", "r") as f:
+                        for each in f.read().split("\n\n"):
+                            hashes.append(each)
+            # announce and write to files if no log exists
             else:
                 new = [x for x in self.feed["entries"]]
                 embeds = get_embed(new)
@@ -57,16 +94,37 @@ class MyClient(discord.Client):
                     for each in self.feed["entries"]:
                         f.write(str(each))
                         f.write("\n\n")
-            new = [x for x in self.feed["entries"] if str(x) not in log]
-            if len(new) > 0:
-                embeds = get_embed(new)
-                for each in embeds:
-                    await announce_channel.send(embed=each)
-                with open("log.txt", "w") as f:
-                    for each in self.feed["entries"]:
+                with open("hashes.txt", "w") as f:
+                    for each in hashes:
                         f.write(str(each))
                         f.write("\n\n")
 
+            new = [x for x in self.feed["entries"] if str(x) not in log and str(get_identifier(x)) not in hashes]
+            modified = [x for x in self.feed["entries"] if str(x) not in log and str(get_identifier(x)) in hashes]
+
+            if len(new) > 0 or len(modified) > 0:
+                try:
+                    newEmbeds = get_embed(new)
+                except Exception as e:
+                    print(str(e))
+                try:
+                    modifiedEmbeds = get_embed_modified(modified)
+                except Exception as e:
+                    print(str(e))
+                try:
+                    embeds = newEmbeds + modifiedEmbeds
+                    for each in embeds:
+                        await announce_channel.send(embed=each)
+                    with open("log.txt", "w") as f:
+                        for each in self.feed["entries"]:
+                            f.write(str(each))
+                            f.write("\n\n")
+                    with open("hashes.txt", "w") as f:
+                        for each in current_hashes:
+                            f.write(str(each))
+                            f.write("\n\n")
+                except Exception as e:
+                    print(str(e))
             await asyncio.sleep(15)
         
 client = MyClient()
